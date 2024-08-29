3e3d9ef4a4674f95378fb553a00f1673aa232ff4
Arian <arian.kulmer@web.de>
2024-08-29 18:58:19 +0200

    unicorn: Fix sonames of extracted blobs
    Change-Id: Ia4327e5b64e705da6dff6da01d2c9f16dfd609c5

--- extract-files.sh
+++ extract-files.sh
@@ -14,8 +14,17 @@ function blob_fixup() {
         vendor/etc/camera/unicorn_enhance_motiontuning.xml|vendor/etc/camera/unicorn_motiontuning.xml)
             sed -i 's/xml=version/xml version/g' "${2}"
             ;;
+        vendor/lib64/hw/fingerprint.goodix_fod.default.so)
+            "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod.default.so" "${2}"
+            ;;
+        vendor/lib64/hw/fingerprint.goodix_fod6.default.so)
+            "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix_fod6.default.so" "${2}"
+            ;;
         vendor/lib64/libcamximageformatutils.so)
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            ;;
+        vendor/lib64/libmialgo_basic.so)
+            "${PATCHELF_0_17_2}" --set-soname "libmialgo_basic.so" "${2}"
             ;;
     esac
 }
